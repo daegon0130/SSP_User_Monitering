49352a57d61648df17b9eedce976cda9b89d94b7
tkdgus96 <tkdgus96@gmail.com>
2021-08-11 15:32:05 +0900

    unpop page fixed

--- frontend/src/components/PVUnpop.vue
+++ frontend/src/components/PVUnpop.vue
@@ -2,19 +2,7 @@
   <v-app>
     <v-container>
       <div>비인기 페이지 조회</div>
-      <v-row justify="center">
-      <v-col cols="4">
-      <v-select
-        v-model="time"
-        :items="items"
-        item-text="dis"
-        item-value="key"
-        label="기간"
-        outlined
-        v-on:change="sendTimeLength(); changedate(); sendDate()"
-    ></v-select>
-    </v-col>
-      </v-row>
+      <date-select-month @receiveDate="receiveDate" @sendTimeLength="receiveTimeLength" @receiveTime="receiveTime" :datetype="'pop'" :timelength="'month'"/>
     <v-btn v-on:click="getData(); load()">
       조회
     </v-btn>
@@ -36,11 +24,14 @@
 
 <script>
 import axios from 'axios'
+import DateSelectMonth from './DateSelectMonth.vue'
 
 export default {
+  components: { DateSelectMonth },
   data () {
     return {
-      time: '2021-08',
+      time: '2021-01',
+      time1: '2021-08',
       loading: true,
       sortBy: 'num',
       sortDesc: false,
@@ -73,12 +64,17 @@ export default {
   },
   methods: {
     async getData () {
-      const res = await axios.post('http://localhost:3000/api/v/page', { time: this.time })
+      // const res = await axios.post('http://localhost:3000/api/v/page', { time: this.time })
+      const res = await axios.post('http://localhost:3000/api/v/page', { startMonth: this.time, endMonth: this.time1 })
       this.datacollection = res.data.elements
       this.loading = false
     },
     load () {
       this.loading = true
+    },
+    receiveDate (date, date2) {
+      this.time = date
+      this.time1 = date2
     }
   },
   mounted () {
